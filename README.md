02b0ce6bc68c02905b418cc8d70e5e031f915d7b
陈 润 <godakid@outlook.com>
2024-04-19 18:40:56 +0800

    docs

--- README.md
+++ README.md
@@ -1,12 +1,19 @@
-# WXT + React
+# OpenAiTranslator
 
+## 功能点实现情况
 - It offers three modes: translation, polishing and summarization.
 - Our tool allows for mutual translation, polishing and summarization across 55 different languages.
 - Streaming mode is supported!
 - It allows users to customize their translation text.
-- One-click copying
-- Text-to-Speech (TTS)
-- Available on all platforms (Windows, macOS, and Linux) for both browsers and Desktop
+- [ ] One-click copying
+- [ ] Text-to-Speech (TTS)
+- [ ] Available on all platforms (Windows, macOS, and Linux) for both browsers and Desktop
 - Support screenshot translation
 - Support for vocabulary books, as well as support for generating memory aids based on the words in the vocabulary books
 - Supports both OpenAI and Azure OpenAI Service at the same time
+
+## 部署引擎
+
+```sh
+docker run -d -p 8080:8080 -p 1337:1337 -p 7900:7900 --shm-size="2g" -v ${PWD}/hardir:/app/hardir hlohaus789/g4f:latest
+```
--- shared/store/index.ts
+++ shared/store/index.ts
@@ -1,6 +1,7 @@
 import {atomWithImmer, useImmerAtom} from "jotai-immer";
-import {atom, createStore} from 'jotai';
+import {atom, createStore, useAtom} from 'jotai';
 import {atomWithStorage, selectAtom} from 'jotai/utils';
+import * as _ from 'underscore';
 
 export interface IPanelStore {
     isOpen: boolean;
@@ -21,9 +22,19 @@ export const getPanelStore = () => appStore.get(PanelStore);
 export function useBrowserAtom<Value extends {}>(
     initialValue: Value
 ) {
-    const storage = atom(async (get) => {
-        const value = atom(await browser.storage.local.get(Object.keys(initialValue)));
-        get(value);
-    }, async (get, set, args) => {
-    });
+    const value = atom(initialValue);
+    const [v, s] = useAtom(value);
+    const storage = atom(
+        async (get) => {
+            s(await browser.storage.local.get(Object.keys(initialValue)) as Value);
+            return get(value);
+        }, async (get, set, new_val) => {
+            const nextValue =
+                _.isFunction(new_val) ? new_val(get(value)) : new_val;
+            await browser.storage.local.set({
+                initialValue: nextValue,
+                ...v,
+            });
+        });
 }
+
