fed6a42c6009b317d4da3aa9eed3f87000b0f69b
godakid <godakid@outlook.com>
2024-04-19 21:34:28 +0800

    可配置 option 的UI

--- entrypoints/popup/App.tsx
+++ entrypoints/popup/App.tsx
@@ -1,14 +1,41 @@
-import { useState } from 'react';
+import React, {useState} from 'react';
 import './App.css';
+import {TitleBarHeader} from "@/shared/components/Logo.tsx";
+import {Input} from "@nextui-org/input";
+import {Button, Divider} from "@nextui-org/react";
+import {EyeFilledIcon, EyeSlashFilledIcon} from "@nextui-org/shared-icons";
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  return (
-    <>
-      {/* 配置 OpenaiKey */}
-    </>
-  );
+    const [isVisible, setIsVisible] = useState(false);
+    const toggleVisibility = () => setIsVisible(isVisible => !isVisible);
+    return (
+        <>
+            <TitleBarHeader></TitleBarHeader>
+            {/* 配置 OpenaiKey */}
+            <Divider></Divider>
+            <div className="flex w-full flex-wrap md:flex-nowrap mb-6 md:mb-0 gap-6px pt-12px  px-6px box-border">
+                <Input size={"sm"} type="text" placeholder="Openai Url"/>
+                <Input size={"sm"}
+                       type={isVisible ? "text" : "password"}
+                       placeholder="Openai Key"
+                       endContent={
+                           <button className="focus:outline-none" type="button" onClick={toggleVisibility}>
+                               {isVisible ? (
+                                   <EyeSlashFilledIcon className="text-2xl text-default-400 pointer-events-none"/>
+                               ) : (
+                                   <EyeFilledIcon className="text-2xl text-default-400 pointer-events-none"/>
+                               )}
+                           </button>
+                       }
+                />
+                <div className={"relative flex justify-end w-full"}>
+                    <Button size="sm" variant={"ghost"}>
+                        Save
+                    </Button>
+                </div>
+            </div>
+        </>
+    );
 }
 
 export default App;
--- entrypoints/popup/main.tsx
+++ entrypoints/popup/main.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import '@/assets/styles/style.less';
 import {TranslatorAppWrapper} from "@/shared/components/App.tsx";
-
+import 'uno.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <TranslatorAppWrapper>
