2e943f1d0e7a6e6d9a9ddfba9c03eaa841377671
chenrun <godakid@outlook.com>
2024-04-28 13:31:50 +0800

    sys

--- entrypoints/options/pages/GeneralSettings.tsx
+++ entrypoints/options/pages/GeneralSettings.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import {Box, CheckIcon, ColorSwatch, Flex, Group, Radio} from "@mantine/core";
 import {IconLighter} from "@tabler/icons-react";
 import {PiBirdLight} from "react-icons/pi";
+import {CgDarkMode} from "react-icons/cg";
 
 const settings = [
     {
@@ -12,10 +13,11 @@ const settings = [
                 label="Select your favorite theme"
                 description="This is anonymous"
                 withAsterisk
+                value={"dark"}
             >
                 <Group mt="xs">
                     <Radio checked={true} icon={PiBirdLight} value="dark" label="dark"/>
-                    <Radio checked={true} icon={CheckIcon} value="light" label="light"/>
+                    <Radio checked={true} icon={CgDarkMode} value="light" label="light"/>
                     <Radio checked={true} icon={CheckIcon} value="system" label="system"/>
                 </Group>
             </Radio.Group>
