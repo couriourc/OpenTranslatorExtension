f4887e85b0bf3f93127681c60a5fca7a6972c23a
chenrun <godakid@outlook.com>
2024-04-28 12:00:03 +0800

    chore(deps): update deps

--- shared/design-pattern/Singleton.ts
+++ shared/design-pattern/Singleton.ts
@@ -1,50 +1,7 @@
 import {AbstractOpenAI} from "@/shared/engines/ABCOpenAiEngine.ts";
+import {Singleton} from "@couriourc/design-patterns";
 
-export class Singleton<T> {
-    #instance: T | null = null;
-    #loaded: boolean = false;
-    #cached: ((instance: T) => any)[] = [];
-
-    set(instance: T): Singleton<T> {
-        if (this.#instance) {
-            return this;
-        }
-        this.#instance = instance;
-        setTimeout(this._loaded.bind(this));
-        return this;
-    }
-
-    replace(instance: T): Singleton<T> {
-        /**/
-        this.#instance = instance;
-        setTimeout(this._loaded.bind(this));
-        return this;
-    }
-
-    get(): T {
-        return this.#instance!;
-    }
-
-    _loaded() {
-        this.#cached.forEach((fn) => {
-            fn(this.#instance!);
-        });
-        this.#cached.length = 0;
-    }
-
-    is_loaded() {
-        return !!this.#instance;
-    }
-
-    then(fn: ((instance: T) => any)): Singleton<T> {
-        if (this.is_loaded()) {
-            fn(this.#instance!);
-            return this;
-        }
-        this.#cached.push(fn);
-        return this;
-    }
-}
+export {Singleton} from "@couriourc/design-patterns";
 
 export const GPTEngine = new Singleton<AbstractOpenAI>();
 
